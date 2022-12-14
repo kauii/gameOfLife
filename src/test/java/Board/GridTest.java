ad51a0af9b9d8dd2c2a4cf8e1f719452d4452338
Dominik Troxler <95303754+trox18@users.noreply.github.com>
2022-12-14 18:48:38 +0100

    Grid exception test implemented

---
+++ src/test/java/Board/GridTest.java
@@ -0,0 +1,22 @@
+package Board;
+
+import Game.Game;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class GridTest {
+
+    @Test
+    void constructor() {
+        Game game = new Game();
+        Exception e = assertThrows(IllegalCallerException.class, () -> new Grid(game, 10));
+
+        String expectedMessage = "Only Board can create a new Grid!!!";
+        String actualMessage = e.getMessage();
+
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
