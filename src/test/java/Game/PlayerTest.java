83648bcbf6aecbd6668f4f887833d7456168d7b0
Dominik Troxler <95303754+trox18@users.noreply.github.com>
2022-12-15 16:52:27 +0100

    PlayerTest added

---
+++ src/test/java/Game/PlayerTest.java
@@ -0,0 +1,46 @@
+package Game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.Objects;
+
+class PlayerTest {
+
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player("Alice", Color.GREEN);
+    }
+
+    @Test
+    void getName() {
+        assert (Objects.equals(player.getName(), "Alice"));
+    }
+
+    @Test
+    void getColor() {
+        assert (player.getColor() == Color.GREEN);
+    }
+
+    @Test
+    void getSetPlayerNr() {
+        player.setPlayerNr(PlayerNr.PLAYER2);
+        assert (player.getPlayerNr() == PlayerNr.PLAYER2);
+    }
+
+    @Test
+    void getLiveCells() {
+        assert (player.getLiveCells() == 0);
+    }
+
+    @Test
+    void addLiveCells() {
+        player.addLiveCells(5);
+        assert (player.getLiveCells() == 5);
+        player.addLiveCells(13);
+        assert (player.getLiveCells() == 18);
+    }
+}
